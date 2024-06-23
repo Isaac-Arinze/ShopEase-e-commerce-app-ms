ad4cc87cf171ca30b8c5075ad3ac67c2e72411a6
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-06-23 20:38:05 +0100

    flyway configured

---
+++ services/product/src/test/java/com/zikan/ecommerce/ProductApplicationTests.java
@@ -0,0 +1,13 @@
+package services.product.src.test.java.com.zikan.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
