acfc7c48f7bf60b2ae92d38119d53ad89409db90
Isaac Sunday Arinze <sunnyanas6@gmail.com>
2024-06-29 15:31:31 +0100

    notification module

--- services/payment/src/main/java/com/zikan/ecommerce/payment/PaymentService.java
+++ services/payment/src/main/java/com/zikan/ecommerce/payment/PaymentService.java
@@ -1,6 +1,7 @@
 package com.zikan.ecommerce.payment;
 
 import com.zikan.ecommerce.notification.NotificationProducer;
+import com.zikan.ecommerce.notification.PaymentNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,9 +10,22 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
-    private NotificationProducer notificationProducer;
+    private final NotificationProducer notificationProducer;
+
     public Integer createPayment(PaymentRequest paymentRequest) {
         var payment = paymentRepository.save(paymentMapper.toPayment(paymentRequest));
+
+        notificationProducer.sendNotification(
+                new PaymentNotificationRequest(
+                        paymentRequest.orderReference(),
+                        paymentRequest.amount(),
+                        paymentRequest.paymentMethod(),
+                        paymentRequest.customer().firstname(),
+                        paymentRequest.customer().lastname(),
+                        paymentRequest.customer().email()
+
+                )
+        );
 
         return payment.getId();
     }
